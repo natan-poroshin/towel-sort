c65bf3e040c3040c71295512426b587df553d4e6
natan-poroshin <natan.poroshin@gmail.com>
2022-08-12 21:59:01 +0300

    feat: implemented towel sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-
-// You should implement your task here.
+  /** 
+ *  @param {Array<Array>} matrix
+ * bracketsConfig = [['(', ')'], ['|', '|']] example
+ */
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if( matrix !== undefined && matrix.length !== 0){
+    return matrix.map(arr => arr.sort((a, b) => a - b))
+      .reduce((prev, curr, index) => index % 2 === 0 ? prev.concat(curr) : prev.concat(curr.reverse()));
+  }
+  else{
+    return [];
+  }
 }
